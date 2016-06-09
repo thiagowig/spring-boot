e008ccf9dda685c75a550b8d6b4307cfaf4205c2
Thiago Fonseca <tfonseca@ciandt.com>
2016-06-09 19:42:30 -0300

    Playing with spring boot

---
+++ src/main/java/br/com/ithiago/Example.java
@@ -0,0 +1,21 @@
+package br.com.ithiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+	}
+	
+}
