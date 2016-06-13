32b685734e2afbbaba41357983adeb6a65ed9ed9
Thiago Fonseca <tfonseca@ciandt.com>
2016-06-13 18:07:21 -0300

    Adding Springfox (Swagger) to the project

--- src/main/java/br/com/ithiago/controller/ProductController.java
+++ src/main/java/br/com/ithiago/controller/ProductController.java
@@ -4,14 +4,16 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ithiago.model.Product;
 import br.com.ithiago.service.ProductService;
 
-@RestController
+@RestController()
 public class ProductController {
 	
 	private static final Logger LOGGER = LogManager.getLogger(ProductController.class);
@@ -19,10 +21,17 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@RequestMapping("/product")
+	@RequestMapping(value="/product", method=RequestMethod.GET)
 	public Product findByDescription(@RequestParam(required=false) String description) throws Exception {
 		LOGGER.log(Level.INFO, "The method findByDescription was called");
 		
 		return productService.findByDescription(description);
+	}
+	
+	@RequestMapping(value="/product/{id}", method=RequestMethod.GET)
+	public String findById(@PathVariable Long id) {
+		LOGGER.log(Level.INFO, "The method findById was called");
+		
+		return "The id is: " + id;
 	}
 }
--- src/main/java/br/com/ithiago/runner/Runner.java
+++ src/main/java/br/com/ithiago/runner/Runner.java
@@ -5,15 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 //@SpringBootApplication(scanBasePackages="br.com.ithiago")
 
-@Configuration("application-context.xml")
+@Configuration()
 @EnableAutoConfiguration
 @ComponentScan(basePackages="br.com.ithiago")
+@EnableSwagger2
 public class Runner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Runner.class, args);
 	}
-	
 }
+
