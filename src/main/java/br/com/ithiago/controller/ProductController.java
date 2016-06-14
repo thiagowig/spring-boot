8856b4ec070cf737f839a69cc6edd3303df8adc4
Thiago Fonseca <dev.thiago@gmail.com>
2016-06-14 19:40:18 -0300

    Refactoring the product service

--- src/main/java/br/com/ithiago/controller/ProductController.java
+++ src/main/java/br/com/ithiago/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ithiago.model.Product;
 import br.com.ithiago.service.ProductService;
 
-@RestController()
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
 	
 	private static final Logger LOGGER = LogManager.getLogger(ProductController.class);
@@ -21,17 +23,22 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@RequestMapping(value="/product", method=RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public Product findByDescription(@RequestParam(required=false) String description) throws Exception {
 		LOGGER.log(Level.INFO, "The method findByDescription was called");
 		
 		return productService.findByDescription(description);
 	}
 	
-	@RequestMapping(value="/product/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public String findById(@PathVariable Long id) {
 		LOGGER.log(Level.INFO, "The method findById was called");
 		
 		return "The id is: " + id;
+	}
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String createProduct(@RequestBody Product product) {
+		return "OK";
 	}
 }
--- src/main/java/br/com/ithiago/service/ProductServiceImpl.java
+++ src/main/java/br/com/ithiago/service/ProductServiceImpl.java
@@ -10,21 +10,25 @@ import br.com.ithiago.util.JsonMapper;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-	private static final String REST_URL = "http://www.mocky.io/v2/575ec8f4110000a7192992dd";
+	public static final String API_URL = "http://localhost:8090/api/product";
 	
-	@Autowired
+	/**
+	 * Use {@link ProductServiceImpl#API_URL} instead.
+	 */
+	@Deprecated
+	public static final String OLD_API_URL = "http://www.mocky.io/v2/575ec8f4110000a7192992dd";
+	
+	
 	private RestClient restClient;
 	
-	public ProductServiceImpl() {
-	}
-	
+	@Autowired
 	public ProductServiceImpl(RestClient restClient) {
 		this.restClient = restClient;
 	}
 
 	@Override
 	public Product findByDescription(String description) throws Exception {
-		String response = restClient.getResponse(REST_URL);
+		String response = restClient.getResponse(API_URL);
 
 		return (Product) JsonMapper.toObject(response, Product.class);
 	}
